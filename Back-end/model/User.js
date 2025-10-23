76d792128b8f62477c93f60064361dcfd2d5cd0c
Harsh-Koundal <aharsh33039@gmail.com>
2025-10-23 08:13:49 -0800

    feat dashboard api

--- Back-end/model/User.js
+++ Back-end/model/User.js
@@ -1,13 +1,27 @@
-import mongoose, { mongo } from 'mongoose';
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema({
-    name:{type:String, required:true},
-    email:{type:String, required:true, unique:true},
-    password:{type:String, required:true},
-    role:{type:String, enum:['user','admin'],
-        default:'user'},
-    verified:{type:Boolean, default:false },
-    createdAt:{type:Date, default:Date.now}
+  fullName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  stream: String,
+  address: String,
+  contactNumber: String,
+  github: String,
+  about: String,
+  favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: "StudyMaterial" }]
+}, { timestamps: true });
+
+// Hash password before save
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
 });
 
-export default mongoose.model('User',userSchema);
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+export default mongoose.model("User", userSchema);
