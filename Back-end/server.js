c281b4edc0a296d9f79c8d56176e1fca43b510eb
Harsh-Koundal <aharsh33039@gmail.com>
2025-10-31 10:10:37 -0800

    bugfix:login

--- Back-end/server.js
+++ Back-end/server.js
@@ -52,11 +52,14 @@ mongoose.connect(MONGODB_URI, {
     process.exit(1);
   });
 
+// --- Middleware ---
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true }));
+
 // --- Routes ---
 app.use('/api/auth', authRoutes);
 app.use('/api/profile', profileRoutes);
 app.use('/api/materials', materialRoutes);
-app.use(express.json({limit:'50mb'}));
 
 
 // --- Health check route ---
